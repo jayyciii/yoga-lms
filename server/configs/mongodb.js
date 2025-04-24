503d822fdc62ce41e8a37a1230512166f44874d1
Nguyen Bao Tran <baotran66773@gmail.com>
2025-04-24 14:55:32 +0700

    connected

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     mongoose.connection.on('connected', ()=> console.log('Database da ket noi'))
 
-    await mongoose.connect(`${process.env.MONGODB_URL}/yogawthtran`)
+    await mongoose.connect(`${process.env.MONGODB_URL}`)
 }
 
 export default connectDB
--- server/controllers/webhook.js
+++ server/controllers/webhook.js
@@ -9,16 +9,17 @@ export const clerkWebhooks = async (req,res)=>{
         await whook.verify(JSON.stringify(req.body), {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
-            "svix-signature": req.headers["svix.signature"]
+            "svix-signature": req.headers["svix-signature"]
         })
 
         const {data,type} = req.body
 
         switch (type) {
             case 'user.created': {
+                console.log(data);
                 const userData = {
                     _id: data.id,
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,  
                 }
